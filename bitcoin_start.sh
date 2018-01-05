dd569592dc16631cb7cfd2c088421be6ff7bffe2
Drew Ripberger <drew.ripberger@gmail.com>
2018-01-04 23:56:41 -0500

    Script to start the compilation process

---
+++ bitcoin_start.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Compilation and test script for the POW(Bitcoin) algorithm"
+
+cores="$(nproc --all)"
+#Navigating to the bitcoin folder
+#cd bitcoin
+echo $cores
+#Starting the compilation process of the daemon
+#./autogen.sh
+#./configure
+#./make -j $cores
