e26c6cd4abb433e7104e4c67a85c5ab20fb5b54d
THE-COB <jmlife6@gmail.com>
2018-01-15 22:19:02 -0500

    Added ChangeFile.py which changes the blocktime in chainparams.cpp

---
+++ ChangeFile.py
@@ -0,0 +1,25 @@
+import re
+import sys
+str1 = "consensus.nPowTargetTimespan ="
+str2 = "consensus.nPowTargetSpacing ="
+def changeFile(secs, deStr):
+	strSecs = str(secs)
+	with open("TEST_chainparams.cpp") as rFile:
+		rText = rFile.read()
+	output = re.search(deStr, rText)
+	startChar = output.span()[1]
+	allSemis = re.finditer(";", rText)
+	endChar = 0
+	for i in allSemis:
+		endChar = i.span()[0]
+		if(endChar > startChar):
+			break;
+	editSect = rText[startChar:endChar]
+	splitFile = rText.split(editSect)
+	editStr = strSecs.join(splitFile)
+	wFile = open("TEST_chainparams.cpp", "w")
+	wFile.write(editStr)
+def dewit(numSecs):
+	changeFile(numSecs, str1)
+	changeFile(numSecs,str2)
+dewit(sys.argv[1])
