d4e99ca59094e0ff2e89007ab256251e2b81f7aa
Drew Ripberger <drew.ripberger@gmail.com>
2018-01-22 23:17:57 -0500

    fixed dep. depends and paths

--- bitcoin_manager.cpp
+++ bitcoin_manager.cpp
@@ -1,21 +1,13 @@
 #include <mpi.h>
 #include <iostream>
 #include <stdlib.h>
-#include <CompUtils.h>
-
+#include <string>
+#include <chrono>
 using namespace std;
 
 const int endTime = 60 * 20; // 20 Minutes
 const int startTime = 5; // 5 Seconds
-int msg;
-
-void ready(int recvNode, int amtReadyNodes){
-	int incoming = 0;
-	while(incoming < amtReadyNodes){
-		MPI_Recv(&msg, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-		incoming++;
-	}
-}
+const string hosts[6] = {"master", "node0", "node1", "node2", "node3", "node4"};
 
 int main(){
 
@@ -30,30 +22,17 @@ int main(){
 	int worldRank;
 	MPI_Comm_rank(MPI_COMM_WORLD, &worldRank);
 
+	system("$HOME/bitcoin/src/bitcoind -regtest -daemon");
+	for(string name : hosts){
+		system("$HOME/bitcoin/src/bitcoin-cli -regtest addnode " + name + " \"add\"");
+	}
+	system("$HOME/bitcoin/src/bitcoin-cli -regtest generate 101")
 
-
-	// Delete bitcoin folder in case old version is found
-	system("rm -rf bitcoin");
-
-	// Clone fresh bitcoin from fork
-	system("git clone git://github.com/drewrip/bitcoin");
+	MPI_Barrier(MPI_COMM_WORLD);
 
 	for(int testTime = startTime; testTime <= endTime; testTime += 5){
 
-		// Compiles bitcoin with new block time
-		system("python3 ChangeFile.py "+testTime);
-		system("./bitcoin/autogen.sh && ./bitcoin/configure && make -j 2");
-
-		
-		if(worldRank == 0){// This node is dedicated to being the master, it will collect and manage data from the nodes
-
-		}
-		else if(worldRank == 1){ // This node is dedicated to being a miner, it will work to confirm blocks
-
-		}
-		else{ // This will catch the rest of the nodes and make them workers that continuously send transactions.
-
-		}
+		MPI_Barrier(MPI_COMM_WORLD);
 	}
 
 
