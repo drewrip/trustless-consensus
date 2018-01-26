7c64aead82c1f876742a2fae62681b3d0f5cd03d
Drew Ripberger <drew.ripberger@gmail.com>
2018-01-26 01:06:15 -0500

    finished standalone manager

--- bitcoin_manager_standalone.cpp
+++ bitcoin_manager_standalone.cpp
@@ -5,11 +5,17 @@
 #include <fstream>
 #include <sstream>
 #include <unistd.h>
+#include <jsoncpp/json/json.h>
+#include <jsoncpp/json/reader.h>
+#include <jsoncpp/json/writer.h>
+#include <jsoncpp/json/value.h>
 using namespace std;
+using namespace std::chrono;
 
 const int endTime = 60 * 10; // 10 Minutes
-const int startTime = 10; // 5 Seconds
-const string sendCommand = "$HOME/bitcoin/src/bitcoin-cli sendtoaddress 1F1tAaz5x1HUXrCNLbtMDqcw6o5GNn4xqX 0.000001";
+const int startTime = 10; // 10 Seconds
+const string sendCommand = "$HOME/bitcoin/src/bitcoin-cli -regtest sendtoaddress miNcNbbqp39Th3QYkiqTXW2QauQ5JKcmjR 0.0001";
+int txs = 0;
 
 std::string sysout(std::string command){
 	std::string out;
@@ -30,27 +36,29 @@ int main(){
 	ofstream data;
 	data.open("bitcoin.csv");
 	data << "Time,TXs\n";
-
 	system("$HOME/bitcoin/src/bitcoind -regtest -daemon");
-	sleep(2);
+	sleep(1);
 	system("$HOME/bitcoin/src/bitcoin-cli -regtest generate 101");
 
-	for(int testTime = startTime; testTime <= endTime; testTime += 5){
-		for(int i = 0; i < 3; i++){
-			std::cout << "Working on " << testTime << "sec..." << std::endl;
-			std::chrono::time_point<std::chrono::system_clock> end = chrono::system_clock::now() + (testTime * 1000);
-			while(chrono::system_clock::now() < end){
+	for(int testTime = startTime; testTime <= endTime; testTime += 10){
+		for(int i = 0; i < 2; i++){
+			std::cout << "Working on " << testTime << "sec... #" << i+1 << "/2"<< std::endl;
+			std::chrono::milliseconds ms(testTime * 1000);
+			std::chrono::time_point<std::chrono::steady_clock> end;
+			end = std::chrono::steady_clock::now() + ms;
+			while(std::chrono::steady_clock::now() < end){
 				system(sendCommand.c_str());
+				txs++;
 			}
-
 			system("$HOME/bitcoin/src/bitcoin-cli -regtest generate 1");
 
 		}
-		
+		/*
 		std::ostringstream lastBlockHash;
 		int maxBlocks = 0;
 		stringstream blockString(sysout("$HOME/bitcoin/src/bitcoin-cli -regtest getblockcount"));
 		blockString >> maxBlocks;
+		std::cout << "Max Blocks: " << maxBlocks << std::endl;
 		std::ostringstream hashCommmand;
 		hashCommmand << "$HOME/bitcoin/src/bitcoin-cli -regtest getblockhash " << maxBlocks - 4;
 		std::string chunkCommand = hashCommmand.str();
@@ -60,7 +68,10 @@ int main(){
 		txStream << "$HOME/bitcoin/src/bitcoin-cli -regtest listsinceblock " << lastHash << " 1";
 		std::string txCommand = txStream.str(); 
 		std::string numTxs = sysout(txCommand);
-		data << testTime << "," << numTxs << "\n";
+		*/
+		data << testTime << "," << txs << "\n";
+		txs = 0;
+
 	}
 
 	// Cleans Up
